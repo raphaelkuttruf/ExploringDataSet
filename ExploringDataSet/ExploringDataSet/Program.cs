a6509117c431452c32d1bdd92fde5036c575984a
Raphael Kuttruf <Raphael.Kuttruf@gmx.net>
2020-07-29 20:24:36 +0200

    Created default console app solutuion

---
+++ ExploringDataSet/ExploringDataSet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExploringDataSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
