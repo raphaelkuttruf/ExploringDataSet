039bcbe9d0d7f4841bdd254677bcffc47805a9df
Raphael Kuttruf <Raphael.Kuttruf@gmx.net>
2021-03-24 20:28:54 +0100

    VS Projekt Release Build angepasst und Code kommentiert

--- ExploringDataSet/ExploringDataSet/Program.cs
+++ ExploringDataSet/ExploringDataSet/Program.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 
+/// <summary>
+/// Implemetierung einer Konsolenanwendung
+/// 
+/// Hier wird ein benutzerdefiniertes System.Data.DataSet erstellt und in einem Anwendungsbeispiel genutzt.
+/// </summary>
 namespace ExploringDataSet
 {
+    /// <summary>
+    /// Programmeinstieg
+    /// 
+    /// Es ist nur ein kleines Konsolenprogramm.
+    /// \image html ConsoleHelloWorld.png
+    /// Bitte bleiben Sie ruhig, falls Sie einen Fehler entdecken.
+    /// Feedback und Fehlerreports sind jeder Zeit willkommen.
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Hauptfunktion des Programms
+        /// 
+        /// Es beginnt mit der Textausgabe "Hello World!"
+        /// </summary>
+        /// <param name="args">Nicht verwendet</param>
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
